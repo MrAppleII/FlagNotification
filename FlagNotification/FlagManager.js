47a19571b663f95b0cb87e414cba3d51053d3041
Jesus Garnica <gtechy100@gmail.com>
2019-07-03 15:01:33 -0700

    Clear Function Added
    Added a way to clear the cache.

--- FlagNotification/FlagManager.js
+++ FlagNotification/FlagManager.js
@@ -1,18 +1,18 @@
 import { EventEmitter } from "events"
 const createID = () => {
-    const pattern = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
-    return pattern.replace(/[xy]/g, (c) => {
-      const r = (Math.random() * 16) | 0;
-      const v = c === 'x' ? r : ((r & 0x3) | 0x8);
-      return v.toString(16);
-    });
-  };
+  const pattern = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+  return pattern.replace(/[xy]/g, c => {
+    const r = (Math.random() * 16) | 0
+    const v = c === "x" ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
 const FlagTypes = {
   ALERT: "alert",
   SUCCESS: "success",
   FAIL: "fail",
   INFO: "info",
-  CHANGE: 'change',
+  CHANGE: "change",
 }
 class FlagManager extends EventEmitter {
   constructor(props) {
@@ -22,21 +22,26 @@ class FlagManager extends EventEmitter {
   }
 
   componentDidMount() {}
-  componentWillUnmount() {
-  
-  }
+  componentWillUnmount() {}
 
   createFlag(notification) {
     const defaultNotify = {
-        id: createID(), // Make a generic ID for the Flag
-      };
-   // console.log("Created Flag")
-    this.noticationList.push(Object.assign(defaultNotify, notification));
-    this.emitChange();
+      id: createID(), // Make a generic ID for the Flag
+    }
+    // console.log("Created Flag")
+    var maxSize = 7
+    if(this.noticationList.length===maxSize){
+      //First pop one
+      this.noticationList.pop()
+
+    }
+
+    this.noticationList.push(Object.assign(defaultNotify, notification))
+    this.emitChange()
   }
   info(message, flagTime) {
     this.createFlag({
-      type:FlagTypes.INFO,
+      type: FlagTypes.INFO,
       message,
       flagTime,
     })
@@ -57,27 +62,37 @@ class FlagManager extends EventEmitter {
   }
   alert(message, flagTime) {
     this.createFlag({
-      type:FlagTypes.ALERT,
+      type: FlagTypes.ALERT,
       message,
       flagTime,
     })
   }
+  clearCache() {
+    this.noticationList.length = 0
+
+    while (this.noticationList.length > 0) {
+      this.noticationList.pop()
+    }
+    this.emitChange()
+  }
   remove(notification) {
-    this.noticationList = this.noticationList.filter(n => notification.id !== n.id);
+    this.noticationList = this.noticationList.filter(
+      n => notification.id !== n.id
+    )
     //console.log("Attempting to remove(Manager)...")
-    this.emitChange();
+    this.emitChange()
   }
 
   emitChange() {
-    this.emit(FlagTypes.CHANGE, this.noticationList);
+    this.emit(FlagTypes.CHANGE, this.noticationList)
   }
 
   addChangeListener(callback) {
-    this.addListener(FlagTypes.CHANGE, callback);
+    this.addListener(FlagTypes.CHANGE, callback)
   }
 
   removeChangeListener(callback) {
-    this.removeListener(FlagTypes.CHANGE, callback);
+    this.removeListener(FlagTypes.CHANGE, callback)
   }
 }
 export default new FlagManager()
